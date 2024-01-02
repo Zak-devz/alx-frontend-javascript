b868494139a0f6462fa47a8f0cf8000ebc82e9a0
Zak-devz <zakariarassili@gmail.com>
2024-01-02 17:47:07 +0100

    add comment to ignore the wrap

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,5 @@
 export default function createIteratorObject(report) {
+  // eslint-disable-next-line wrap-iife
   return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
