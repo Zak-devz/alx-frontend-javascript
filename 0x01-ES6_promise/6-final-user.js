7cdf38d968c36c29b53bdfff26c6e52f667480aa
Zak-devz <zakariarassili@gmail.com>
2024-01-02 19:31:51 +0100

    task 6 completed

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    }))
+  );
+}
