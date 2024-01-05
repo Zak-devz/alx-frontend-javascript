348c80ed12ac77db8c0b52ce9ed6a0c402234731
Zak-devz <zakariarassili@gmail.com>
2024-01-06 00:30:24 +0100

    fix eslint  errors

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
     if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMesagge !== 'function'
+      this.constructor !== Building
+      && typeof this.evacuationWarningMesagge !== 'function'
     ) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
+        'Class extending Building must override evacuationWarningMessage',
       );
     }
     this._sqft = sqft;
