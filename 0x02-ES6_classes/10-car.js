e53b2ed9b2e14ed077a04f2c7008358129483427
Zak-devz <zakariarassili@gmail.com>
2024-01-06 00:49:41 +0100

    update eslint errors

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,45 +1,16 @@
-class Car {
-  constructor(brand, motor, color) {
+export default class Car {
+  constructor(brand = null, motor = null, color = null) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
   cloneCar() {
-    const clone = Object.create(Car.prototype);
+    const clone = new this.constructor();
     const symbols = Object.getOwnPropertySymbols(this);
-
     symbols.forEach((symbol) => {
-      const descriptor = Object.getOwnPropertyDescriptor(this, symbol);
-      Object.defineProperty(clone, symbol, descriptor);
+      clone[symbol] = this[symbol];
     });
-
     return clone;
   }
 }
-
-export default Car;
