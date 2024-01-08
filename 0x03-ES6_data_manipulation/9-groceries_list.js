802b2a6a3776f686f29317c7a82dff1ce35c778d
Zak-devz <zakariarassili@gmail.com>
2024-01-08 16:19:42 +0100

    task completedd

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* eslint-disable implicit-arrow-linebreak */
+const groceriesList = () =>
+  new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+export default groceriesList;
